b826b4d34a890cea8e7ae3249d02882a8f90c655
Maryjane Watanabe <maryjanekw@gmail.com>
2025-11-09 13:48:29 -0800

    Create Signature drink class #18

---
+++ src/main/java/com/pluralsight/drinks/SignatureDrink.java
@@ -0,0 +1,19 @@
+package com.pluralsight.drinks;
+
+public class SignatureDrink extends Drink{
+
+    public SignatureDrink(String size){
+        super("", size, 5);
+    }
+
+    @Override
+    public double calculatedPrice(){
+        double sizeMultiplier = switch (size.toLowerCase()){
+            case "small" -> 1.0;
+            case "medium" -> 1.4;
+            case "large" -> 1.6;
+            default -> 1.0;
+        };
+        return basePrice * sizeMultiplier;
+    }
+}
