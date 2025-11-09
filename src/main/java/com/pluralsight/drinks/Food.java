2cb890750e316ab395f584d132ee11c3c3a6c808
Maryjane Watanabe <maryjanekw@gmail.com>
2025-11-09 15:56:51 -0800

    Create food class #9

---
+++ src/main/java/com/pluralsight/drinks/Food.java
@@ -0,0 +1,20 @@
+package com.pluralsight.drinks;
+
+public class Food {
+
+    private String name;
+    private double price;
+
+    public Food(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
