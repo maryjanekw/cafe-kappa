cac6cedef1f0de13c538e8d95f5862d2de49c45f
Maryjane Watanabe <maryjanekw@gmail.com>
2025-11-09 16:21:16 -0800

    fixing signature drink pricing

--- src/main/java/com/pluralsight/drinks/SignatureDrink.java
+++ src/main/java/com/pluralsight/drinks/SignatureDrink.java
@@ -3,15 +3,15 @@ package com.pluralsight.drinks;
 public class SignatureDrink extends Drink{
 
     public SignatureDrink(String size){
-        super("", size, 5);
+        super("", size, 6);
     }
 
     @Override
     public double calculatedPrice(){
         double sizeMultiplier = switch (size.toLowerCase()){
             case "small" -> 1.0;
-            case "medium" -> 1.4;
-            case "large" -> 1.6;
+            case "medium" -> 1.25;
+            case "large" -> 1.5;
             default -> 1.0;
         };
         return basePrice * sizeMultiplier;
