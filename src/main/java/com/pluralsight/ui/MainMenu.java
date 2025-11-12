f9799ff5f340cfa0326e92d62b1b8d9db9a77192
Maryjane Watanabe <maryjanekw@gmail.com>
2025-11-12 14:08:26 -0800

    Main menu skeleton

--- src/main/java/com/pluralsight/ui/MainMenu.java
+++ src/main/java/com/pluralsight/ui/MainMenu.java
@@ -1,5 +1,49 @@
 package com.pluralsight.ui;
 
+
+import java.util.Scanner;
+
 public class MainMenu {
 
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        boolean running = true;
+
+        while (running) {
+            // Main menu display
+            System.out.println("===== Welcome to Café Kappa =====");
+            System.out.println("1) Start New Order");
+            System.out.println("2) Display Menu");
+            System.out.println("0) Exit");
+            System.out.print("Choose an option: ");
+
+            // input validator
+            int choice = getValidInt(read);
+
+            switch (choice) {
+                case 1 -> { // New order
+                    System.out.println("\nStarting a new order...\n");
+                }
+                case 2 ->{ // Menu display
+
+                }
+                case 0 -> {
+                    running = false;
+                    System.out.println("Goodbye!");
+                }
+                default -> System.out.println("Invalid choice. Try again.");
+            }
+        }
+    }
+
+    // Input validation Helper
+    private static int getValidInt(Scanner scanner) {
+        while (true) {
+            try {
+                return Integer.parseInt(scanner.nextLine().trim());
+            } catch (NumberFormatException e) {
+                System.out.print("Please enter a number: ");
+            }
+        }
+    }
 }
